38a33bb49aea7eac56f5ad6b4df0a9693d9cdd57
Geir Ove Myhr <gomyhr@gmail.com>
2015-04-04 07:45:07 +0200

    Remove source tag on objects and change source:date to nvdb:date

--- elveg2osm.py
+++ elveg2osm.py
@@ -274,13 +274,11 @@ def create_osmtags(elveg_tags):
     # All ways should have a TRANSID (will change to LOKALID with SOSI 4.5)
     osmtags['nvdb:id'] = elveg_tags['TRANSID']
 
-    # Add source date
+    # Add date from NVDB
     if elveg_tags.has_key('DATAFANGSTDATO'):
         date = elveg_tags['DATAFANGSTDATO']
-        osmtags['source:date'] = '%s-%s-%s' % (date[0:4],date[4:6],date[6:8])
+        osmtags['nvdb:date'] = '%s-%s-%s' % (date[0:4],date[4:6],date[6:8])
     
-    osmtags['source'] = 'Kartverket Elveg'
-
     return osmtags
 
 def parse_lanes(lane_string):
